02d6993669c2505230e959464882a34c471a4826
dgreene-r7 <dgreene@rapid7.com>
2016-12-14 16:40:07 -0500

    Added binary + error handling so you don't play yourself.

--- index.js
+++ index.js
@@ -2,22 +2,35 @@
 
 var rp = require('request-promise-native');
 const imageToAscii = require("image-to-ascii");
+const path = require('path');
 
 const options = {
   uri: 'https://www.instagram.com/aceitunagrill/media/',
   transform: (body) => JSON.parse(body)
 };
 
+const promisify = (func) => {
+  return new Promise((resolve, reject) => {
+    func(function handler(err, result) {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
+}
+
 const imgOpts = {
   bg: true,
   pixels: ' '
 };
 
-const req = rp(options).then((body) => {
+module.exports = rp(options).then((body) => {
   const items = body.items;
   const recent = items[0];
 
-  imageToAscii(recent.images.standard_resolution.url, imgOpts, (err, converted) => {
-    console.log(err || converted);
-  });
-}).catch((err) => {});
+  return promisify((f) => imageToAscii(recent.images.standard_resolution.url, imgOpts, f));
+}).catch((err) => {
+    const img = path.resolve(__dirname, 'img/khaled.png');
+    return promisify((f) => imageToAscii(img, imgOpts, f));
+});
